7d9f31e2a6cf6a69c93810738442b573c4fad67c
nature.xie <nature@ref.finance>
2023-08-02 12:38:35 +0800

    update error words

--- src/ZKEVM/AAVE.Modal.RepayModal.jsx
+++ src/ZKEVM/AAVE.Modal.RepayModal.jsx
@@ -744,7 +744,7 @@ return (
             </div>
             {state.needApprove && (
               <Widget
-                src={`rref-bigboss.near/widget/ZKEVM.AAVE.ModalPrimaryButton`}
+                src={`guessme.near/widget/ZKEVM.AAVE.ModalPrimaryButton`}
                 props={{
                   config,
                   loading: state.loading,
