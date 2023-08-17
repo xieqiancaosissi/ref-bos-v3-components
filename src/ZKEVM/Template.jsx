89675eb33bb3df90fa9788ac83286c99edfa7f4e
Max <max@ref.finance>
2023-08-17 21:27:24 +0800

    publish zkevm all in one

--- src/ZKEVM/Template.jsx
+++ src/ZKEVM/Template.jsx
@@ -1,34 +1,47 @@
-
 const Container = styled.div`
-  display:flex;
-  flex-wrap:wrap;
-  gap:30px 36px;
-  color:#fff;
-`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px 36px;
+  color: #fff;
+`;
 const templates = [
   {
-    src: 'guessme.near/widget/ZKEVMSwap.zkevm-bridge',
-    bannerImg: 'https://ipfs.near.social/ipfs/bafkreievrd4imhglvabvg2vgcqxmpk4vog6p3dw5cy3rsay4ocsyu3y7w4',
+    src: "bluebiu.near/widget/ZKEVM-all-in-one",
+    bannerImg:
+      "https://ipfs.near.social/ipfs/bafkreicwk2fqkrtkwhyioz7l6tkgmduxsw4ey7t6pd7isahu3bex7etbgu",
   },
   {
-    src: 'guessme.near/widget/ZKEVMSwap.zkevm-swap',
-    bannerImg: 'https://ipfs.near.social/ipfs/bafkreihwkwijwsazh5dp3rxc2lwgi4algusd3b447o3xcfcnndbgaura3q',
+    src: "guessme.near/widget/ZKEVMSwap.zkevm-bridge",
+    bannerImg:
+      "https://ipfs.near.social/ipfs/bafkreievrd4imhglvabvg2vgcqxmpk4vog6p3dw5cy3rsay4ocsyu3y7w4",
   },
   {
-    src: 'guessme.near/widget/ZKEVM.GAMMA',
-    bannerImg: 'https://ipfs.near.social/ipfs/bafkreih77ecmgng2fpy6mblk2aipt3agwm3anjldcyktx2j6kssggpelue',
+    src: "guessme.near/widget/ZKEVMSwap.zkevm-swap",
+    bannerImg:
+      "https://ipfs.near.social/ipfs/bafkreihwkwijwsazh5dp3rxc2lwgi4algusd3b447o3xcfcnndbgaura3q",
   },
   {
-    src: 'guessme.near/widget/ZKEVM.AAVE',
-    bannerImg: 'https://ipfs.near.social/ipfs/bafkreibmqzfwki7zye6ruqi3hifgexs6g5nv4qvzcmrm73lnra5mbkdrxe',
-  }
-]
-return <Container>
+    src: "guessme.near/widget/ZKEVM.GAMMA",
+    bannerImg:
+      "https://ipfs.near.social/ipfs/bafkreih77ecmgng2fpy6mblk2aipt3agwm3anjldcyktx2j6kssggpelue",
+  },
   {
-    templates.map(({src, bannerImg}, index) => <Widget src="guessme.near/widget/ZKEVM.Template-card" key={index} props={{
-      src,
-      bannerImg,
-    }}/>)
-  }
-  
-</Container>
+    src: "guessme.near/widget/ZKEVM.AAVE",
+    bannerImg:
+      "https://ipfs.near.social/ipfs/bafkreibmqzfwki7zye6ruqi3hifgexs6g5nv4qvzcmrm73lnra5mbkdrxe",
+  },
+];
+return (
+  <Container>
+    {templates.map(({ src, bannerImg }, index) => (
+      <Widget
+        src="guessme.near/widget/ZKEVM.Template-card"
+        key={index}
+        props={{
+          src,
+          bannerImg,
+        }}
+      />
+    ))}
+  </Container>
+);
